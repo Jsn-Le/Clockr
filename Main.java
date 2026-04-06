a5e20fc7594db41185c054d6ec5389e01bacb9fa
Jason Le <jason.le0804@gmail.com>
2026-04-05 19:53:19 -0500

    clock in button functionality

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ public class Main {
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(500,1000);
 
-            JPanel recordsPanel = new JPanel();
+            JPanel recordsPanel = new JPanel(new BorderLayout());
             JPanel controlPanel = new JPanel();
             JTextField textfield = new JTextField(25);
             JButton clockInButton = new JButton("Clock In");
@@ -21,6 +21,27 @@ public class Main {
             controlPanel.add(textfield);
             controlPanel.add(clockInButton);
             controlPanel.add(clockOutButton);
+
+            ClockRecordManager clockRecordManager = new ClockRecordManager();
+            ClockRecordJTable clockRecordJTable = new ClockRecordJTable(clockRecordManager);
+            JTable jTable = new JTable(clockRecordJTable);
+            JScrollPane scrollPane = new JScrollPane(jTable);
+            
+            recordsPanel.add(scrollPane);
+
+            clockInButton.addActionListener(e -> {
+                String employee = textfield.getText();
+                employee = employee.trim();
+
+                if (employee.isEmpty()) {
+                    return;
+                }
+
+                clockRecordManager.addRecord(employee);
+                clockRecordJTable.fireTableDataChanged();
+
+                textfield.setText("");
+            });
 
             frame.setVisible(true);
 
